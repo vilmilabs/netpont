0ea8d0e370c169896b55d164edc54befccc8291d
VilmaoWebServices <webservices00@hotmail.com>
2025-08-06 17:01:05 +0200

    netpontv1_dev01

---
+++ README.md
@@ -0,0 +1,2 @@
+Vilmao NETPONT (v1.netpont.hu)
+Version 1
---
+++ app/v1/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const v1_return = () => {
+  return (
+    <div>v1_return</div>
+  )
+}
+
+export default v1_return
